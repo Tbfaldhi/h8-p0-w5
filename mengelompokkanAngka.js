4e8900c65aa7cfe9faa1b07a581cdcd82bed1b06
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-01-24 13:57:14 +0700

    tugas1week5

---
+++ mengelompokkanAngka.js
@@ -0,0 +1,38 @@
+/*
+1.init kelompokAngka sebagai variabel array kosong
+2.buat looping untuk memasukkan array kosong sebanyak 3 kedalam array kelompokAngka
+3.buat looping untuk membaca isi array dari kiri ke kanan
+    3a. jika array[j] adalah kelipatan 3 maka input ke array kelompok angka dengan index kolom 2
+    3b. jika array[j] adalah angka genap  maka input array ke array kelompok angka dengnan index kolom 1
+    3c. jika array[j] adalah angka ganjil maka input array ke array kelompok angka dengan index kolom 0
+*/
+
+function mengelompokkanAngka(arr) {
+  var kelompokArr = [];
+
+  for (var i = 0; i < 3; i++) {
+    kelompokArr.push([]);
+  }
+  //console.log(kelompokArr);
+  for (var j = 0; j < arr.length; j++) {
+    if (arr[j] % 3 === 0) {
+      //console.log(kelompokArr);
+      kelompokArr[2].push(arr[j]);
+    } else {
+      if (arr[j] % 2 === 0) {
+        kelompokArr[0].push(arr[j]);
+      }else{if(arr[j]%2!==0){
+        kelompokArr[1].push(arr[j]);
+       }
+      }
+    }
+  }
+
+  return kelompokArr;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]

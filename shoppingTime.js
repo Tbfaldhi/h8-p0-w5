a91f91ff5438f0b83b53016b198a54bf4c6f7f38
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-01-28 22:27:23 +0700

    tambah pseudocode

--- shoppingTime.js
+++ shoppingTime.js
@@ -1,54 +1,73 @@
 /*
-
+init variabel untuk harga barang sepatu Stacattu, baju zoro,Casing Handphone, dan baju H&N
+init variabel untuk sisa dengan nilai awal sama dengan money untuk menghasilkan nilai changeMoney
+init var cart sebagai array kosong untuk menampung barang yang akan dibeli.
+init var objProduct untuk defenisi kelompok objek yang mewakili memberId,money,listBarang, dan changeMoney
+    jika member id adalah '' maka 'Mohon maaf, toko X hanya berlaku untuk member saja'
+    jika money kurang dari 15000 maka Mohon maaf, uang tidak cukup
+    jika nilai shoppingTime kosong adalah kosong atau undefined maka cetak 'Mohon maaf, toko X hanya berlaku untuk member saja'
+        jika nilai kurang  atau sama dengan 1500000 maka push barang sepatu Stacattu ke cart dan kurangi money dengan harga barang
+        jika nilai kurang  atau sama dengan 500000 maka push barang Baju Zoro ke cart dan kurangi money dengan harga barang
+        jika nilai kurang  atau sama dengan 250000 maka push barang Baju H&N ke cart dan kurangi money dengan harga barang
+        jika nilai kurang  atau sama dengan 175000 maka push barang Sweater Uniklooh ke cart dan kurangi money dengan harga barang
+        jika nilai kurang  atau sama dengan 50000 maka push barang Casing Handphone ke cart dan kurangi money dengan harga barang
 */
+
+
+
 function shoppingTime(memberId, money) {
 
-var sisa=money;
+
 var Stacattu = 1500000;
 var Zoro = 500000;
 var HN = 250000;
 var Uniklooh = 175000;
 var Handphone = 50000;
+var sisa=money;
 var cart=[];
+var objProduct={};
+    objProduct.memberId=memberId;
+    objProduct.money=money;
+    objProduct.listBarang=cart;
+    objProduct.changeMoney=sisa;
 
- if(money>1500000){
+
+
+if(memberId===''){
+  return 'Mohon maaf, toko X hanya berlaku untuk member saja';
+}else{
+  if(money<=15000){
+    return 'Mohon maaf, uang tidak cukup';
+  }else{
+    if(memberId===undefined && money=== undefined){
+      return 'Mohon maaf, toko X hanya berlaku untuk member saja';
+    }
+  }
+}
+
+if(money>=Stacattu){
    sisa = money-Stacattu;
    cart.push('Sepatu Stacattu');
   }
-      if(money>500000){
+      if(money>=Zoro){
         sisa-=Zoro;
           cart.push('Baju Zoro');
         }
-        if(money>250000){
+        if(money>=HN){
          sisa-=HN;
          cart.push('Baju H&N');
         }
-        if(money>175000){
+        if(money>=Uniklooh){
         sisa-=Uniklooh;
         cart.push('Sweater Uniklooh');
         }
-        if(money>=50000){
+        if(money>=Handphone){
           sisa-=Handphone;
           cart.push('Casing Handphone');
         }
 
-  if(memberId===''){
-    return 'Mohon maaf, toko X hanya berlaku untuk member saja';
-  }else{
-    if(money<=15000){
-      return 'Mohon maaf, uang tidak cukup';
-    }else{
-      if(memberId===undefined && money=== undefined){
-        return 'Mohon maaf, toko X hanya berlaku untuk member saja';
-      }
-    }
-  }
 
-  var objProduct={};
-  objProduct.memberId=memberId;
-  objProduct.money=money;
-  objProduct.listBarang=cart;
-  objProduct.changeMoney=sisa;
+
   //return memberId;
   return objProduct;
 }
